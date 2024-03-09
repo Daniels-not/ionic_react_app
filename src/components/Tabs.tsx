c96f74a0f90656ac3cda09776b22dbc4d6fd2e7b
Ramy Campusano Volquez <ramydcampusano@hotmail.com>
2024-03-08 20:32:46 -0600

    hover effect as been added ⚙️

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -16,37 +16,61 @@ function Tabs() {
         <IonContent>
           <ul className="ion-padding">
             <li className="ion-margin-bottom">
-              <IonRouterLink routerLink="/" routerDirection="none" className="ion-link flex items-center space-x-2 text-sm">
+              <IonRouterLink
+                routerLink="/"
+                routerDirection="none"
+                className="ion-link flex items-center space-x-2 text-sm py-2 px-4 rounded hover:bg-gray-200 active:bg-gray-300"
+              >
                 <IonIcon icon={flameOutline} className="text-3xl" />
                 <IonLabel className="text-base">Home</IonLabel>
               </IonRouterLink>
             </li>
             <li className="ion-margin-bottom">
-              <IonRouterLink routerLink="/tabs/News" routerDirection="none" className="ion-link flex items-center space-x-2 text-sm">
+              <IonRouterLink
+                routerLink="/tabs/News"
+                routerDirection="none"
+                className="ion-link flex items-center space-x-2 text-sm py-2 px-4 rounded hover:bg-gray-200 active:bg-gray-300"
+              >
                 <IonIcon icon={newspaperOutline} className="text-3xl" />
                 <IonLabel className="text-base">News</IonLabel>
               </IonRouterLink>
             </li>
             <li className="ion-margin-bottom">
-              <IonRouterLink routerLink="/tabs/Weather" routerDirection="none" className="ion-link flex items-center space-x-2 text-sm">
+              <IonRouterLink
+                routerLink="/tabs/Weather"
+                routerDirection="none"
+                className="ion-link flex items-center space-x-2 text-sm py-2 px-4 rounded hover:bg-gray-200 active:bg-gray-300"
+              >
                 <IonIcon icon={waterOutline} className="text-3xl" />
                 <IonLabel className="text-base">Weather</IonLabel>
               </IonRouterLink>
             </li>
             <li className="ion-margin-bottom">
-              <IonRouterLink routerLink="/tabs/UniversitySearch" routerDirection="none" className="ion-link flex items-center space-x-2 text-sm">
+              <IonRouterLink
+                routerLink="/tabs/UniversitySearch"
+                routerDirection="none"
+                className="ion-link flex items-center space-x-2 text-sm py-2 px-4 rounded hover:bg-gray-200 active:bg-gray-300"
+              >
                 <IonIcon icon={schoolOutline} className="text-3xl" />
                 <IonLabel className="text-base">University Search</IonLabel>
               </IonRouterLink>
             </li>
             <li className="ion-margin-bottom">
-              <IonRouterLink routerLink="/tabs/AgePrediction" routerDirection="none" className="ion-link flex items-center space-x-2 text-sm">
+              <IonRouterLink
+                routerLink="/tabs/AgePrediction"
+                routerDirection="none"
+                className="ion-link flex items-center space-x-2 text-sm py-2 px-4 rounded hover:bg-gray-200 active:bg-gray-300"
+              >
                 <IonIcon icon={personOutline} className="text-3xl" />
                 <IonLabel className="text-base">Age Prediction</IonLabel>
               </IonRouterLink>
             </li>
             <li className="ion-margin-bottom">
-              <IonRouterLink routerLink="/tabs/GenderPredictor" routerDirection="none" className="ion-link flex items-center space-x-2 text-sm">
+              <IonRouterLink
+                routerLink="/tabs/GenderPredictor"
+                routerDirection="none"
+                className="ion-link flex items-center space-x-2 text-sm py-2 px-4 rounded hover:bg-gray-200 active:bg-gray-300"
+              >
                 <IonIcon icon={accessibilityOutline} className="text-3xl" />
                 <IonLabel className="text-base">Gender Prediction</IonLabel>
               </IonRouterLink>
